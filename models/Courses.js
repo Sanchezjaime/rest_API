a3d8807b34f459cbeb12c20b2d013209685a32e6
Jaime Sanchez <sanchezjaime@protonmail.com>
2022-06-05 22:10:55 -0700

    added validation to user and course models

--- models/Courses.js
+++ models/Courses.js
@@ -1,6 +1,9 @@
 'use strict';
+const { Model } = require("sequelize");
+const bcrypt = require('bcrypt');
 const Sequelize = require('sequelize');
 
+//model layout
 module.exports = (sequelize) => {
   class Courses extends Sequelize.Model {}
   Courses.init({
@@ -12,10 +15,26 @@ module.exports = (sequelize) => {
     title: {
       type: Sequelize.STRING,
       allowNull: false,
+      validate: {
+        notNull: {
+          msg: "A title is required."
+        },
+        notEmpty: {
+          msg: "A title is required."
+        }
+      }
     },
     description: {
       type: Sequelize.TEXT,
       allowNull: false,
+      validate: {
+        notNull: {
+          msg: "A description is required."
+        },
+        notEmpty: {
+          msg: "A description is required."
+        }
+      }
     },
     estimatedTime: {
       type: Sequelize.STRING,
@@ -28,8 +47,13 @@ module.exports = (sequelize) => {
   }, { sequelize });
 
   Courses.associate = (models) => {
-    Courses.belongsTo(models.Users);
+    Courses.belongsTo(models.Users, {
+      foreignKey: {
+        fieldName: 'userId',
+        allowNull: false,
+      }
+    })
   }
 
   return Courses;
-}
+};
--- models/Users.js
+++ models/Users.js
@@ -1,5 +1,8 @@
 'use strict';
+const { Model } = require('sequelize');
+const bcrypt = require('bcrypt');
 const Sequelize = require('sequelize');
+
 
 module.exports = (sequelize) => {
   class Users extends Sequelize.Model {}
@@ -12,23 +15,65 @@ module.exports = (sequelize) => {
     firstName: {
       type: Sequelize.STRING,
       allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'A first name is required.'
+        },
+        notEmpty: {
+          msg: 'Please provide first name.'
+        },
+      }
     },
     lastName: {
       type: Sequelize.STRING,
       allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'A last name is required.'
+        },
+        notEmpty: {
+          msg: 'Please provide a last name.'
+        },
+      }
     },
     emailAddress: {
       type: Sequelize.STRING,
       allowNull: false,
+      unique: {
+        msg: 'This email is already in use.'
+      },
+      validate: {
+        isEmail: {
+          msg: 'Please provide a valid email.'
+        },
+        notNull: {
+          msg: 'Email required.'
+        },
+      }
     },
     password: {
       type: Sequelize.STRING,
       allowNull: false,
+      set(val) {
+        const hashedPassword = bcrypt.hasSync(val, 10);
+        this.setDataValue('password', hashedPassword);
+      },
+      validate: {
+        notNull: {
+          msg: 'Password required.'
+        },
+        notEmpty: {
+          msg: 'Please provide password.'
+        },
+      }
     },
   }, { sequelize });
 
   Users.associate = (models) => {
-    Users.hasMany(models.Courses);
+    Users.hasMany(models.Courses, {
+      foreignKey: 'userId',
+      allowNull: false,
+    });
   }
 
   return Users;
